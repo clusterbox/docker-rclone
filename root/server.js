c02514685369bff68d78e85b57f2e79e2690d6cd
Brad Davis <brad@transparentproperties.com>
2021-02-27 23:47:24 -0800

    WIP create temp directory for rclone move process so we can run multiple commands at once

--- root/server.js
+++ root/server.js
@@ -33,16 +33,28 @@ log4js.configure({
 var rclone_move_logger = log4js.getLogger('rclone_move');
 app.post('/rclone_move', function (req, res){
 
-    var rCloneSyncCommand = process.env.SYNC_COMMAND + " --config=/rclone/rclone.conf" + " --log-file=/logs/rclone_move.log --min-age 1m --delete-after";
-    rclone_move_logger.info("rclone move command starting: ", rCloneSyncCommand);
+    //Command to create the temp folder we're going to use to store the data we will move with rClone
+    var tmpFolderPath = 'mkdir ' + process.env.RCLONE_SOURCE + 'tmp_' +  Date.now();
+    rclone_move_logger.info("tmpFolderPath: ", tmpFolderPath);
 
-    exec(rCloneSyncCommand, function(error, stdout, stderr) {
-        error ? rclone_move_logger.error("RCLONE MOVE COMMAND error: ", error) : rclone_move_logger.info("RCLONE MOVE COMMAND done: ", stdout, stderr);
+    //Command to move the files into our temp processing folder
+    //TODO: Move everything except folders that begin with "processing_*"
+    var moveToTmpFolderCmd = 'mv  -v ' + process.env.RCLONE_SOURCE + '* ' + tmpFolderPath;
+    rclone_move_logger.info("moveToTmpFolderCmd: ", moveToTmpFolderCmd);
 
-        //Clean up empty folders
-        var removeEmptyDirs = 'find . -depth -type d -exec rmdir {} \\; 2>/dev/null';
-        exec(removeEmptyDirs, {'cwd': '/local_media'});
-    });
+    //rClone command to upload the temp folder contents to the cloud
+    var rCloneSyncCommand = process.env.RCLONE_ENV + ' ' + process.env.RCLONE_COMMAND + ' ' + tmpFolderPath + ' ' + process.env.RCLONE_DEST + ' ' + process.env.RCLONE_FLAGS;
+    rclone_move_logger.info("NEW rclone move command starting: ", rCloneSyncCommand);
+
+
+
+    // exec(rCloneSyncCommand, function(error, stdout, stderr) {
+    //     error ? rclone_move_logger.error("RCLONE MOVE COMMAND error: ", error) : rclone_move_logger.info("RCLONE MOVE COMMAND done: ", stdout, stderr);
+    //
+    //     //Clean up empty folders
+    //     var removeEmptyDirs = 'find . -depth -type d -exec rmdir {} \\; 2>/dev/null';
+    //     exec(removeEmptyDirs, {'cwd': '/local_media'});
+    // });
 
     res.send('rclone move started');
 });
