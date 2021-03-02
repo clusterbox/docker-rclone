3412b908a72a0730e011c7bfa28bedcc184f879c
Brad Davis <brad@transparentproperties.com>
2021-03-01 23:29:38 -0800

    WIP attempting to correct bug that causes encfs folders to fall out of sync

--- root/server.js
+++ root/server.js
@@ -33,24 +33,7 @@ app.post('/rclone_move', function (req, res) {
   var removeEmptyDirs = 'find . -depth -type d -exec rmdir {} \\; 2>/dev/null';
 
   //Define temp directory we're going to use to store the data we will move with rClone
-  var timestamp = Date.now();
-  var tmpDir = process.env.RCLONE_UNENCRYPTED_MEDIA + 'rclone_upload_' + timestamp;
-
-  //Define the encrypted temp directory name
-  var getEncfsDirCmd = 'ENCFS6_CONFIG="/encfs_config/encfs.xml" encfs --extpass="encfs_config/encfspass" encfsctl encode ' + process.env.RCLONE_UNENCRYPTED_MEDIA + ' rclone_upload_' + timestamp ;
-  rclone_move_logger.info("!!!!getEncfsDirCmd: ", getEncfsDirCmd);
-  exec(getEncfsDirCmd, function (error, stdout, stderr) {
-    if (error) {
-      rclone_move_logger.error("getEncfsDirCmd COMMAND error: ", error);
-      return;
-
-    } else {
-      rclone_move_logger.info("DONE getEncfsDirCmd: ", stdout, stderr);
-    }
-  });
-
-
-  var tmpDirEncrypted = process.env.RCLONE_ENCRYPTED_MEDIA + 'rclone_upload_' + Date.now();
+  var tmpDir = process.env.RCLONE_UNENCRYPTED_MEDIA + 'rclone_upload_' + Date.now();
 
   //Command to create the temp directory
   var makeTmpDirCmd = 'mkdir ' + tmpDir;
@@ -58,51 +41,70 @@ app.post('/rclone_move', function (req, res) {
   //Command to move the correct files into our temp directory
   var moveToTmpDirCmd = 'find ' + process.env.RCLONE_UNENCRYPTED_MEDIA + '* -prune ! -name rclone_upload_* -exec mv {} ' + tmpDir  + '/. +';
 
-  //rClone command to upload the temp directory contents to the cloud
-  var rCloneSyncCommand = process.env.RCLONE_ENV + ' ' + process.env.RCLONE_COMMAND + ' ' + tmpDirEncrypted + ' ' + process.env.RCLONE_DEST + ' ' + process.env.RCLONE_FLAGS;
+  //Command to use encFS lib to GET the encrypted temp directory name
+  var getEncfsDirCmd = 'ENCFS6_CONFIG=/encfs_config/encfs.xml encfsctl encode /local ' + process.env.RCLONE_CONTAINER_TYPE + '/' + tmpDir + ' --extpass="cat /encfs_config/encfspass"';
 
   //Run all our commands now
-  rclone_move_logger.info("RUNNING removeEmptyDirs: ", removeEmptyDirs);
-  exec(removeEmptyDirs, {'cwd': process.env.RCLONE_UNENCRYPTED_MEDIA}, function (error, stdout, stderr) {
+  rclone_move_logger.info("RUNNING getEncfsDirCmd: ", getEncfsDirCmd);
+  exec(getEncfsDirCmd, function (error, stdout, stderr) {
     if (error) {
-      rclone_move_logger.error("removeEmptyDirs COMMAND error: ", error);
+      rclone_move_logger.error("getEncfsDirCmd COMMAND error: ", error);
       return;
 
     } else {
-      rclone_move_logger.info("DONE removeEmptyDirs: ", stdout, stderr);
-      rclone_move_logger.info("RUNNING makeTmpDirCmd: ", makeTmpDirCmd);
-      exec(makeTmpDirCmd, function (error, stdout, stderr) {
+      rclone_move_logger.info("DONE getEncfsDirCmd: ", stdout, stderr);
 
+      var tmpDirEncrypted = process.env.RCLONE_ENCRYPTED_MEDIA + stdout;
+
+      rclone_move_logger.info("RUNNING removeEmptyDirs: ", removeEmptyDirs);
+      exec(removeEmptyDirs, {'cwd': process.env.RCLONE_UNENCRYPTED_MEDIA}, function (error, stdout, stderr) {
         if (error) {
-          rclone_move_logger.error("makeTmpDirCmd COMMAND error: ", error);
+          rclone_move_logger.error("removeEmptyDirs COMMAND error: ", error);
           return;
 
         } else {
-          rclone_move_logger.info("DONE makeTmpDirCmd: ", stdout, stderr);
-          rclone_move_logger.info("RUNNING moveToTmpDirCmd: ", moveToTmpDirCmd);
-          exec(moveToTmpDirCmd, function (error, stdout, stderr) {
+          rclone_move_logger.info("DONE removeEmptyDirs: ", stdout, stderr);
+          rclone_move_logger.info("RUNNING makeTmpDirCmd: ", makeTmpDirCmd);
+          exec(makeTmpDirCmd, function (error, stdout, stderr) {
 
             if (error) {
-              rclone_move_logger.error("moveToTmpDirCmd COMMAND error: ", error);
+              rclone_move_logger.error("makeTmpDirCmd COMMAND error: ", error);
               return;
 
             } else {
-              rclone_move_logger.info("DONE moveToTmpDirCmd: ", stdout, stderr);
-              rclone_move_logger.info("RUNNING rCloneSyncCommand: ", rCloneSyncCommand);
-              exec(rCloneSyncCommand, function (error, stdout, stderr) {
-                error ? rclone_move_logger.error("rCloneSyncCommand COMMAND error: ", error) : rclone_move_logger.info("DONE rCloneSyncCommand: ", stdout, stderr);
+              rclone_move_logger.info("DONE makeTmpDirCmd: ", stdout, stderr);
+              rclone_move_logger.info("RUNNING moveToTmpDirCmd: ", moveToTmpDirCmd);
+              exec(moveToTmpDirCmd, function (error, stdout, stderr) {
 
-                //Clean up empty folders
-                exec(removeEmptyDirs, {'cwd': process.env.RCLONE_UNENCRYPTED_MEDIA});
+                if (error) {
+                  rclone_move_logger.error("moveToTmpDirCmd COMMAND error: ", error);
+                  return;
+
+                } else {
+
+                  //rClone command to upload the temp directory contents to the cloud
+                  var rCloneSyncCommand = process.env.RCLONE_ENV + ' ' + process.env.RCLONE_COMMAND + ' ' + tmpDirEncrypted + ' ' + process.env.RCLONE_DEST + ' ' + process.env.RCLONE_FLAGS;
+
+                  rclone_move_logger.info("DONE moveToTmpDirCmd: ", stdout, stderr);
+                  rclone_move_logger.info("RUNNING rCloneSyncCommand: ", rCloneSyncCommand);
+                  exec(rCloneSyncCommand, function (error, stdout, stderr) {
+                    error ? rclone_move_logger.error("rCloneSyncCommand COMMAND error: ", error) : rclone_move_logger.info("DONE rCloneSyncCommand: ", stdout, stderr);
+
+                    //Clean up empty folders
+                    exec(removeEmptyDirs, {'cwd': process.env.RCLONE_UNENCRYPTED_MEDIA});
+                  });
+
+                }
+
               });
 
             }
 
           });
-
         }
 
       });
+
     }
   });
 
